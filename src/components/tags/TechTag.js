6c1ad9101faaa96fe9c9fc597528f5c70e425eb7
banyh <banyhong@gliacloud.com>
2019-10-26 22:00:16 +0800

    support all icons

--- src/components/tags/TechTag.js
+++ src/components/tags/TechTag.js
@@ -1,5 +1,12 @@
 import React from "react"
 import * as FontAwesome from "react-icons/fa"
+import * as Ionicons from "react-icons/io"
+import * as MaterialDesign from "react-icons/md"
+import * as Typicons from "react-icons/ti"
+import * as GithubOcticons from "react-icons/go"
+import * as Feather from "react-icons/fi"
+import * as GameIcons from "react-icons/gi"
+import * as WeatherIcons from "react-icons/wi"
 import * as Devicons from "react-icons/di"
 
 import "./tags.css"
@@ -8,7 +15,15 @@ import { Link } from "gatsby";
 const TechTag = (props) => {
     const { tag, tech, name, size, color } = props
     const str = name;
-    const icon = /^Fa/.test(str) ? React.createElement(FontAwesome[name]) : React.createElement(Devicons[name]);
+    const icon = /^Fa/.test(str) ? React.createElement(FontAwesome[name]) :
+                 /^Io/.test(str) ? React.createElement(Ionicons[name]) :
+                 /^Md/.test(str) ? React.createElement(MaterialDesign[name]) :
+                 /^Ti/.test(str) ? React.createElement(Typicons[name]) :
+                 /^Go/.test(str) ? React.createElement(GithubOcticons[name]) :
+                 /^Fi/.test(str) ? React.createElement(Feather[name]) :
+                 /^Gi/.test(str) ? React.createElement(GameIcons[name]) :
+                 /^Wi/.test(str) ? React.createElement(WeatherIcons[name]) :
+                 React.createElement(Devicons[name]);
 
     return (
         <div className="d-inline-block p-1">
